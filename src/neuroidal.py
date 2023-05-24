9eb3edce08e4fe3a69c501f9edb04abc06af270f
Chandradeep Chowdhury <chandradeep.choudhury@gmail.com>
2023-05-24 10:34:19 -0700

    structured repo

---
+++ src/neuroidal.py
@@ -0,0 +1,34 @@
+import gc, os, sys
+from math import comb
+import numpy as np
+from numpy.random import *
+import graph_tool as gt
+
+
+seed(42)
+np.random.seed(42)
+gt.seed_rng(42)
+
+
+"""TODO: refactor the notebook functions into this class"""
+
+
+class Neuroidal:
+    def __init__(self):
+        pass
+
+    # Create the graph with the properties
+    def create(self):
+        pass
+
+    # Check and fire
+    def _check_and_fire(self):
+        pass
+
+    # Check for interference
+    def _interference_check(self):
+        pass
+
+    # JOIN function
+    def JOIN_one_step_shared(self):
+        pass
